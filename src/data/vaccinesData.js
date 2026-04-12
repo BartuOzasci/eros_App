ba211a8e31a92731f2ab7dfd42453202e93d18ca
Bartu Özaşçı <bartuozasci@gmail.com>
2026-04-12 23:49:10 +0300

    Update vaccinesData.js

--- src/data/vaccinesData.js
+++ src/data/vaccinesData.js
@@ -2,7 +2,7 @@ export const vaccinesData = [
   { id: 1, name: "Bronşin Aşısı", date: "2026-04-06", isCompleted: true },
   { id: 2, name: "Köpek Karma Aşısı", date: "2026-05-05", isCompleted: false },
   { id: 3, name: "Kuduz Aşısı", date: "2026-06-10", isCompleted: false },
-  { id: 4, name: "Kuduz Aşısı", date: "2026-04-15", isCompleted: false },
+  { id: 4, name: "Kuduz Aşısı", date: "2026-06-15", isCompleted: false },
   { id: 5, name: "Lyme Aşısı", date: "2026-06-15", isCompleted: false },
   { id: 6, name: "İç Dış Parazit", date: "2026-01-02", isCompleted: true },
   { id: 7, name: "İç Dış Parazit", date: "2026-03-09", isCompleted: true },
